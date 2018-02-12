7529d3bd4c5467bf63a66daf6d5941932cd3f6c0
Tilmann Stehle <stehle@informatik.uni-hamburg.de>
2018-02-12 16:59:29 +0100

    Added default methods to obtain parameter-Descriptions of IHasParameter-Instances

--- src/de/unihamburg/masterprojekt2016/traceability/IHasParameter.java
+++ src/de/unihamburg/masterprojekt2016/traceability/IHasParameter.java
@@ -5,4 +5,31 @@ import java.util.List;
 public interface IHasParameter {
 	List<Parameter> getParameters();
 	void addParameter(Parameter parameter);
+	default String getOrderedParameterTypes()
+	{
+		StringBuilder stringBuilder= new StringBuilder();
+		for (Parameter parameter : this.getParameters())
+		{
+			stringBuilder.append(parameter.getType() + ", ");
+		}
+		if (this.getParameters().size() > 0)
+		{
+			stringBuilder.setLength(stringBuilder.length() - 2);
+		}
+		return stringBuilder.toString();
+	}
+
+	default String getOrderedParametersAsString()
+	{
+		StringBuilder stringBuilder= new StringBuilder();
+		for (Parameter parameter : this.getParameters())
+		{
+			stringBuilder.append(parameter.getName()+" "+parameter.getType()  + ", ");
+		}
+		if (this.getParameters().size() > 0)
+		{
+			stringBuilder.setLength(stringBuilder.length() - 2);
+		}
+		return stringBuilder.toString();
+	}
 }
